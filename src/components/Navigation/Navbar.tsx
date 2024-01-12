07abb4a4e7b5e5930667c218d9b12013f1726558
Danish Teuku <teukutiga@gmail.com>
2024-01-12 21:01:03 +0800

    refactor: Fix nav semantics

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -2,13 +2,13 @@ import MaxWidthContainer from "../UI/MaxWidthContainer";
 
 function Navbar() {
   return (
-    <nav>
-      <header>
+    <header>
+      <nav>
         <div className="hidden md:block sticky h-20 w-full border-b backdrop-blur-sm text-foreground">
           <MaxWidthContainer>Navbar</MaxWidthContainer>
         </div>
-      </header>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
